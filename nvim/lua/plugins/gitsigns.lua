d3a632a513d13c3ff93aa7a4b9763a31cfc91850
jonasuj <jonas@ulsnes.dk>
2021-04-06 23:10:09 +0200

    Add new gitsigns mappings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -19,6 +19,8 @@ require'gitsigns'.setup{
     ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
     ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
     ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+    ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+    ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
   },
   current_line_blame = true,
 }
