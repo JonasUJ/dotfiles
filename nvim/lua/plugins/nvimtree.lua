7590da5e9eb9e429f9205b07877a281f27337bc3
JonasUJ <jonas@ulsnes.dk>
2021-08-22 21:41:42 +0200

    Offset bufferline on toggle file tree

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,4 +1,15 @@
-vim.api.nvim_set_keymap('n', '<Leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.__toggle = function ()
+    if require'nvim-tree.view'.win_open{any_tabpage=true} then
+        require'bufferline.state'.set_offset(0)
+        require'nvim-tree'.close()
+    else
+        require'bufferline.state'.set_offset(31, '')
+        require'nvim-tree'.find_file(true)
+    end
+end
+
+vim.api.nvim_set_keymap('n', '<Leader>t', '<CMD>lua vim.__toggle()<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<Leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' } -- empty by default, don't auto open tree on specific filetypes.
 vim.g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
