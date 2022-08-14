cb28f89c28094874e112342a2f57173111c1de4e
JonasUJ <jonas@ulsnes.dk>
2022-08-14 23:05:30 +0200

    Improve lsp_toggle_lines

--- nvim/lua/plugins/lsp_lines.lua
+++ nvim/lua/plugins/lsp_lines.lua
@@ -1,11 +1,25 @@
 require "lsp_lines".setup {}
 
 function _G.lsp_lines_toggle()
-    vim.diagnostic.config {
-        virtual_lines = {
-            only_current_line = not vim.diagnostic.config().virtual_lines.only_current_line,
+    local val = vim.diagnostic.config().virtual_lines
+
+    if type(val) == "boolean" then
+        if val then
+            vim.diagnostic.config {
+                virtual_lines = false,
+            }
+        else
+            vim.diagnostic.config {
+                virtual_lines = {
+                    only_current_line = true,
+                },
+            }
+        end
+    else
+        vim.diagnostic.config {
+            virtual_lines = true,
         }
-    }
+    end
 end
 
 Map("n", "<Leader>e", "<cmd>lua _G.lsp_lines_toggle()<CR>", opts)
