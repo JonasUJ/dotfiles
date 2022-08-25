ab88a86064bf27b49654c5258881cb679ee36818
JonasUJ <jonas@ulsnes.dk>
2022-08-25 17:54:46 +0200

    Add WezTerm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+wezterm = require "wezterm"
+
+return {
+    color_scheme = "tokyonight",
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
+}
