f6de18f12f1c45176b360312471bc688b5f683c0
JonasUJ <jonas@ulsnes.dk>
2022-08-31 13:24:57 +0200

    Add silent to barbar

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,30 +1,32 @@
+local opts = { silent = true }
+
 -- Move to previous/next
-Map("n", "<A-,>", ":BufferPrevious<CR>")
-Map("n", "<A-.>", ":BufferNext<CR>")
+Map("n", "<A-,>", ":BufferPrevious<CR>", opts)
+Map("n", "<A-.>", ":BufferNext<CR>", opts)
 
 -- Re-order to previous/next
-Map("n", "<A-<>", ":BufferMovePrevious<CR>")
-Map("n", "<A->>", ":BufferMoveNext<CR>")
+Map("n", "<A-<>", ":BufferMovePrevious<CR>", opts)
+Map("n", "<A->>", ":BufferMoveNext<CR>", opts)
 
 -- Goto buffer in position...
-Map("n", "<A-1>", ":BufferGoto 1<CR>")
-Map("n", "<A-2>", ":BufferGoto 2<CR>")
-Map("n", "<A-3>", ":BufferGoto 3<CR>")
-Map("n", "<A-4>", ":BufferGoto 4<CR>")
-Map("n", "<A-5>", ":BufferGoto 5<CR>")
-Map("n", "<A-6>", ":BufferGoto 6<CR>")
-Map("n", "<A-7>", ":BufferGoto 7<CR>")
-Map("n", "<A-8>", ":BufferGoto 8<CR>")
-Map("n", "<A-9>", ":BufferLast<CR>")
+Map("n", "<A-1>", ":BufferGoto 1<CR>", opts)
+Map("n", "<A-2>", ":BufferGoto 2<CR>", opts)
+Map("n", "<A-3>", ":BufferGoto 3<CR>", opts)
+Map("n", "<A-4>", ":BufferGoto 4<CR>", opts)
+Map("n", "<A-5>", ":BufferGoto 5<CR>", opts)
+Map("n", "<A-6>", ":BufferGoto 6<CR>", opts)
+Map("n", "<A-7>", ":BufferGoto 7<CR>", opts)
+Map("n", "<A-8>", ":BufferGoto 8<CR>", opts)
+Map("n", "<A-9>", ":BufferLast<CR>", opts)
 
 -- Close buffer
-Map("n", "<A-c>", ":BufferClose<CR>")
+Map("n", "<A-c>", ":BufferClose<CR>", opts)
 
 -- Pin buffer
-Map("n", "<A-p>", ":BufferPin<CR>")
+Map("n", "<A-p>", ":BufferPin<CR>", opts)
 
 -- Magic buffer-picking mode
-Map("n", "<A-b>", ":BufferPick<CR>")
+Map("n", "<A-b>", ":BufferPick<CR>", opts)
 
 G.bufferline = {
     animation = false,
