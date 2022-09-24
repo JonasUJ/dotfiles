22c669f7ada9132b066cf1de4a72c59bb227125d
JonasUJ <jonas@ulsnes.dk>
2022-09-24 21:05:01 +0200

    Switch tree side

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -68,7 +68,7 @@ require "nvim-tree".setup {
         -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
         width = 30,
         -- side of the tree, can be one of "left" | "right" | "top" | "bottom"
-        side = "left",
+        side = "right",
         -- if true the tree will resize itself after opening a file
         mappings = {
             -- custom only false will merge the list with the default mappings
