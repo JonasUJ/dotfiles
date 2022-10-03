d1e61f89b572016dd9d8c777624043371b43e6b2
JonasUJ <jonas@ulsnes.dk>
2022-10-03 15:49:47 +0200

    Remove update

--- fish/functions/update.fish
+++
@@ -1,8 +0,0 @@
-function update
-    sudo test 1
-    rustup update &
-    brew update && brew upgrade &
-    sudo apt update && sudo apt upgrade -y &
-    sudo apt autoremove -y
-    nvim +PackerSync +qall
-end
